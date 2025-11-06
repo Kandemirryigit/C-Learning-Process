1502640d6d47e603c3997718362357122ad0dc40
yiğit kandemir <kandemirryigit@gmail.com>
2025-11-06 19:47:04 +0300

    Add files via upload

---
+++ main17.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+// I can create an array like this
+// And I can reach that array with loop
+
+int main()
+{
+    int numbers[5]={1,2,3,4,5};
+    for (int i=0;i<5;i++)
+    {
+        printf("\n%d",numbers[i]);
+    }
+
+}
