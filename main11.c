15c73477f239ed26bd5595ae06f03ab9a1b8968c
yiğit kandemir <kandemirryigit@gmail.com>
2025-11-06 15:47:46 +0300

    Add files via upload

---
+++ main11.c
@@ -0,0 +1,17 @@
+/*
+Useful syntax for using if and else
+*/
+
+
+#include  <stdio.h>
+
+
+// This is an easy syntax for using if and else 
+// condition ? if true: if false
+int main()
+{
+    int number1=10;
+    int number2=20;
+
+    number2>number1 ? printf("number2 is greater than number1") : printf("Number1 is greater than number1");
+}
