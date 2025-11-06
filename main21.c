72bbcad801a4c51febf33146f797a150a27c5be1
yiğit kandemir <kandemirryigit@gmail.com>
2025-11-06 20:56:21 +0300

    Update main21.c

--- main21.c
+++ main21.c
@@ -21,12 +21,16 @@ void function2(void)
     printf("World");
 }
 
+// I didn't return anything inside function1 and function2 because their type is void
+
 
 // I can call functions like this
 int main()
 {
     function1();
     function2();
+
+    return 0;  // I returned 0 because function's type is int.0 means program runned succesfully
     
 }
 
@@ -34,4 +38,5 @@ int main()
 // It's good to create the functions above main function 
 // Because when I call that functions my main function knows about them
 // If I create them below main function and don't initilaze them above main function main can't know them and program crashes
+
 // If I create the functiopns below main function then I need to initalize them above main 
