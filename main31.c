463bceb15cbe0e82595d10e94111e9e1dbb622d4
yiğit kandemir <kandemirryigit@gmail.com>
2025-11-09 17:21:32 +0300

    Add files via upload

---
+++ main31.c
@@ -0,0 +1,20 @@
+/*
+- Learning booalean values in c
+*/
+
+#include <stdio.h>
+
+
+// In c programming language we can use int values for boolean 
+// 0 means false
+// the other values except 0 is mean true
+
+
+int main()
+{
+    if(32)
+        printf("We are going to see this line on the screen");
+    
+    if(0)
+        printf("We are not going to see this line on the screen");
+}
